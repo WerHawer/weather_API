68fdf1ad6d8b21ad003a48f4c298f16848bc0998
WerHawer <werhawer@gmail.com>
2020-03-26 17:49:48 +0200

    Updates

---
+++ precache-manifest.548f18b7208907630a84004eb44353fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c2e132c1203fe0a40359d2c97bb909",
+    "url": "/weather_API/index.html"
+  },
+  {
+    "revision": "3061256cb55373502e55",
+    "url": "/weather_API/static/css/main.80a9d711.chunk.css"
+  },
+  {
+    "revision": "8a7adcdf8fe264694632",
+    "url": "/weather_API/static/js/2.1760777b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_API/static/js/2.1760777b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3061256cb55373502e55",
+    "url": "/weather_API/static/js/main.09270a1f.chunk.js"
+  },
+  {
+    "revision": "43f8da282e4ccb68f820",
+    "url": "/weather_API/static/js/runtime-main.ad4c0810.js"
+  }
+]);
