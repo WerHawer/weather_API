18244dedd3943c0ed5c9336a5a5233c8f21bd72c
WerHawer <werhawer@gmail.com>
2020-04-02 19:23:11 +0300

    Updates

--- precache-manifest.37873a99bb6af95051dd7e2a9f313f8b.js
+++ precache-manifest.37873a99bb6af95051dd7e2a9f313f8b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "89ce8cf132c0c84293d03f48c946c572",
+    "revision": "724e209b894266468f085cfc55e5f6ba",
     "url": "/weather_API/index.html"
   },
   {
-    "revision": "9b39f014ab2879dbd578",
+    "revision": "02cda1a4d36ca10c43f6",
     "url": "/weather_API/static/css/main.9fcbe6ba.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/weather_API/static/js/2.82638be5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9b39f014ab2879dbd578",
-    "url": "/weather_API/static/js/main.a76f01f2.chunk.js"
+    "revision": "02cda1a4d36ca10c43f6",
+    "url": "/weather_API/static/js/main.61d57f8b.chunk.js"
   },
   {
     "revision": "43f8da282e4ccb68f820",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather_API/precache-manifest.df0b89ef38c22bbffbc72b969e06fd6f.js"
+  "/weather_API/precache-manifest.37873a99bb6af95051dd7e2a9f313f8b.js"
 );
 
 self.addEventListener('message', (event) => {
